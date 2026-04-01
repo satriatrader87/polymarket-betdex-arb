041291588773135c9ae1fa361ffd126ce9467096
satriatrader87 <167980755+satriatrader87@users.noreply.github.com>
2026-04-01 09:17:55 +0700

    Update main.py

--- main.py
+++ main.py
@@ -1,1 +1,16 @@
+# main.py
+import asyncio
+from core.orchestrator import ArbOrchestrator
 
+async def main():
+    # Inisialisasi Orchestrator
+    bot = ArbOrchestrator()
+    
+    # Jalankan proses scanning
+    try:
+        await bot.start_scanning()
+    except KeyboardInterrupt:
+        print("\n🛑 Bot dihentikan oleh pengguna.")
+
+if __name__ == "__main__":
+    asyncio.run(main())
